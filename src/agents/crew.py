fc25248c42117cd3820b642669313b45a3b02ab7
Reynxzz <luthfirey258@gmail.com>
2025-01-05 18:50:02 +0700

    fix bug on assigining tool

--- src/agents/crew.py
+++ src/agents/crew.py
@@ -1,21 +1,16 @@
-# src/agents/crew.py
 from typing import Optional
-from crewai import Crew, Task, Process, LLM
-from langchain_openai import ChatOpenAI
+from crewai import Crew, Task, Process
 import os
-
-# Import agents
 from agents.requirements import RequirementAgent
 from agents.researcher import ResearcherAgent
 from agents.data_analyst import DataAnalysisAgent
 from agents.coder import CoderAgent
-from tools.search_cortex import CortexSearchRequirementsTool, DocumentProcessor, DocumentType
+from tools.search_cortex import CortexSearchRequirementsTool, CortexSearchTechnicalTool, DocumentProcessor, DocumentType
 from tools.get_snowflake_tables import SnowflakeTableTool
 from custom_cortex_llm.snowflake_mistral_agents import CrewSnowflakeLLM
 
 def create_crew(prompt: str, docs_uploaded: bool, docs_path: Optional[str] = None):
     """Create and configure the agent crew"""
-    # Initialize Snowflake session
     from snowflake.snowpark.session import Session
     from dotenv import load_dotenv
     
@@ -34,42 +29,25 @@ def create_crew(prompt: str, docs_uploaded: bool, docs_path: Optional[str] = Non
     
     snowpark_session = Session.builder.configs(connection_params).create()
 
-    # llm = LLM(
-    #     model="mistral/mistral-large-latest",
-    # ),
-
     llm = CrewSnowflakeLLM(
         session=snowpark_session,
         model_name="mistral-large2",
-        temperature=0.5,
+        temperature=0.3,
     )
 
-    # llm = LLM(
-    #     model="gemini/gemini-1.5-flash",
-    #     temperature=0.7
-    # )
-    
-    # llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0.7)
-
-    # Create search tools
     search_req_tool = CortexSearchRequirementsTool(snowpark_session, result_as_answer=True)
-    search_tech_tools = CortexSearchRequirementsTool(snowpark_session, result_as_answer=True)
+    search_tech_tools = CortexSearchTechnicalTool(snowpark_session, result_as_answer=True)
     analysis_tools = SnowflakeTableTool(snowpark_session, result_as_answer=True)
 
-    # Initialize agents
     requirement_agent = RequirementAgent(llm, [search_req_tool])
     data_agent = DataAnalysisAgent(llm, [analysis_tools])    
     researcher_agent = ResearcherAgent(llm, [search_tech_tools])
     coder_agent = CoderAgent(llm)
-    # validator_agent = ValidatorAgent(llm)
     
-    # Process documents if provided
     if docs_path:
         doc_processor = DocumentProcessor(snowpark_session)
-        # Use the DocumentType enum instead of string
         doc_processor.process_document(docs_path, DocumentType.REQUIREMENTS)
     
-    # Define tasks
     requirement_task = Task(
         description=f"""Analyze the business requirements input prompt and document using the Search Requirements Documents tool.
         Input: {prompt}.
@@ -99,7 +77,7 @@ def create_crew(prompt: str, docs_uploaded: bool, docs_path: Optional[str] = Non
            Example: you can search with query="Find tables related to rides and drivers"
         3. Analyze the returned table structures and sample data
         4. Map the available tables and fields to the specific requirements""",
-        expected_output="""Provide a detailed analysis report containing:
+        expected_output="""If there Snowflake data needed or can be used:
         1. Detailed mapping between business requirements and available data fields. Don't make up any table names or columns that are not available. If not neccesary to use table, just say no data needed. 
         2. Recommendations for data usage in implementation and example how can we use it in python. If not neccesary to use table, just say no data needed.""",
         agent=data_agent,
@@ -125,20 +103,20 @@ def create_crew(prompt: str, docs_uploaded: bool, docs_path: Optional[str] = Non
         expected_output="""
         scikit-learn requirements' implementation code in python. If not neccessary to use scikit-learn, just say this requirements dont need scikit-learn.""",
         agent=researcher_agent,
-        context=[requirement_task],
+        context=[requirement_task, data_analysis_task],
         tools=[search_tech_tools]
         )
     
     researcher_streamlit_task = Task(
-        description="""Research Streamlit implementation details based on REQUIREMENTS using the Search Technical Documentation tool.
+        description="""Research Streamlit implementation details using the `search_tech_tools`.
         
-        Context: Use outputs from both previous requirements and scikit-learn research tasks (if available).
+        Context: Use outputs from previous requirements and scikit-learn research tasks (if available).
         
         Steps:
         1. For each UI component needed to fulfill the original requirements:
         - Use the Search Technical Documentation tool with:
             - query: Component requirements and sklearn integration points
-            - doc_type: "technical_docs"
+            - doc_type: "technical_docs" (you can only use this parameter to use the tools)
             - tech_stack: "streamlit" (you can only use this parameter to use the tools)
         2. Ensure UI patterns align with both business requirements and implementation (sklearn if necesseary)
         3. If requirements need some common python library to run the code (other than streamlit e.g. PyPDF, etc.), Provide the implementation code by your own knowledge""",
@@ -166,9 +144,8 @@ def create_crew(prompt: str, docs_uploaded: bool, docs_path: Optional[str] = Non
     
     tasks = [requirement_task, data_analysis_task, researcher_sklearn_task, researcher_streamlit_task, coder_task]
     
-    # Create crew
     crew = Crew(
-        agents=[requirement_agent, data_agent, researcher_agent, coder_agent], #validator_agent
+        agents=[requirement_agent, data_agent, researcher_agent, coder_agent],
         tasks=tasks,
         process=Process.sequential,
         verbose=True
